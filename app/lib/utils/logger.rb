191fc893b19551896b696f1991b070410a1e945f
sugamasao <sugamasao@gmail.com>
2009-10-11 03:27:49 +0900

    write docoment

--- app/lib/utils/logger.rb
+++ app/lib/utils/logger.rb
@@ -3,6 +3,11 @@
 require 'java/util/logging'
 
 module Utils
+
+  # == 概要
+  # Java の Logger ラッパークラスです。
+  # Ruby 風にログを出力できるよう、ログレベルのラッピングも行っています。
+  # @author sugamasao
   class Logger
     LEVELS = {
       :fatal => Java::Util::Logging::Level::SEVERE,
@@ -12,37 +17,53 @@ module Utils
       :debug => Java::Util::Logging::Level::FINE,
     }
 
-    # 
+    # == 概要
+    # Logger クラスを生成します。
+    # @param [String] logger_name 生成するログの名称[省略可]（どう使われるかよくわからん）
     def initialize(logger_name = self.class.name)
       @logger = Java::Util::Logging::Logger::getLogger(logger_name)
     end
 
-    # Level::SEVERE 相当
+    # == 概要
+    # Level::SEVERE 相当のログを出力します
+    # @param [Object] *args ログ出力メッセージ（複数指定及び省略可能）
     def fatal(*args)
       write(:fatal, args)
     end
     
-    # Level::SEVERE 相当
+    # == 概要
+    # Level::SEVERE 相当のログを出力します
+    # @param [Object] *args ログ出力メッセージ（複数指定及び省略可能）
     def error(*args)
       write(:error, args)
     end
     
-    # Level::WARNING 相当
+    # == 概要
+    # Level::WARNING 相当のログを出力します
+    # @param [Object] *args ログ出力メッセージ（複数指定及び省略可能）
     def warn(*args)
       write(:warn, args)
     end
 
-    # Level::INFO 相当
+    # == 概要
+    # Level::INFO 相当のログを出力します
+    # @param [Object] *args ログ出力メッセージ（複数指定及び省略可能）
     def info(*args)
       write(:info, args)
     end
 
-    # Level::FINE 相当
+    # == 概要
+    # Level::FINE 相当のログを出力します
+    # @param [Object] *args ログ出力メッセージ（複数指定及び省略可能）
     def debug(*args)
       write(:debug, args)
     end
 
     :private
+    # == 概要
+    # ログメッセージを実際に出力します
+    # @param [Symbol] level ログレベルを現すシンボル
+    # @param [Array] args 出力メッセージ
     def write(level, args)
 
       # 先頭のメッセージ部分を取得
