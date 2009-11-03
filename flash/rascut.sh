7f4c47ce1d9b3dbab1969dcf47f22fa28d09f2d8
sugamasao <sugamasao@gmail.com>
2009-11-03 17:40:49 +0900

    add

--- flash/rascut.sh
+++ flash/rascut.sh
@@ -2,5 +2,8 @@
 
 CURRENT=`dirname ${0}` 
 
-rascut -s --log=${HOME}/Library/Preferences/Macromedia/Flash\ Player/Logs/flashlog.txt ${CURRENT}/src/HatarakiZakari.mxml -c"-load-config+=${CURRENT}/build/HatarakiZakari.xml -define CONFIG::DEBUG true -output ${CURRENT}/bin/HatarakiZakari.swf" -m "${CURRENT}/test=test" -I ${CURRENT}/bin/HatarakiZakari.swf
+#ruby ${CURRENT}/test/data/server.rb &
+
+rascut -s -v  --log=${HOME}/Library/Preferences/Macromedia/Flash\ Player/Logs/flashlog.txt ${CURRENT}/src/HatarakiZakari.mxml -c"-load-config+=${CURRENT}/build/HatarakiZakari.xml -define CONFIG::DEBUG true -output ${CURRENT}/bin/HatarakiZakari.swf" -m "${CURRENT}/test=test" --swf-path ${CURRENT}/bin/HatarakiZakari.swf 
+
 
